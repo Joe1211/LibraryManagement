7fd310e0a77b417747416549a060879d520c8cd8
Null <974902238@qq.com>
2018-11-26 14:24:19 +0800

    11.26更改

--- src/main/java/com/wonders/shixi/controller/GetPerson.java
+++ src/main/java/com/wonders/shixi/controller/GetPerson.java
@@ -2,6 +2,12 @@ package com.wonders.shixi.controller;
 
 import com.wonders.shixi.pojo.Person;
 import com.wonders.shixi.service.PersonService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiResponse;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,12 +18,20 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import java.util.List;
 
 @Controller
+@Api(value = "人模块")
 public class GetPerson {
+
     @Autowired
     PersonService personService;
+
+    private static final Logger log= LogManager.getLogger(GetPerson.class.getName());
+
     @RequestMapping(value="/getPerson.do",method = RequestMethod.GET)
     @ResponseBody
+
     public Person getPerson(@RequestParam("name") String name){
+        log.info(name);
+        log.error(name);
       Person person=  new Person();
       person.setName(name);
       return person;
@@ -37,8 +51,6 @@ public class GetPerson {
         List<Person> persons=personService.postAllPerson();
         persons.add(person);
         return persons;
-
-
 
     }
 }
